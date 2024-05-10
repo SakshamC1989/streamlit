dfd6a9c04c5e4c8d2a05f4893da7427dc13c389f
SakshamC1989 <saksham.om@gmail.com>
2024-05-11 00:40:03 +0100

    adding options

---
+++ Options.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import yfinance as yf
+import datetime
+import streamlit as st
+
+def options_chain(symbol):
+
+    tk = yf.Ticker(symbol)
+    # Expiration dates
+    exps = tk.options
+
+    # Get options for each expiration
+    options = pd.DataFrame()
+    for e in exps:
+        opt = tk.option_chain(e)
+        opt = pd.concat([opt.calls,opt.puts])
+        opt['expirationDate'] = e
+        options = pd.concat([options,opt], ignore_index=True)
+
+    # Bizarre error in yfinance that gives the wrong expiration date
+    # Add 1 day to get the correct expiration date
+    options['expirationDate'] = pd.to_datetime(options['expirationDate']) + datetime.timedelta(days = 1)
+    options['dte'] = (options['expirationDate'] - datetime.datetime.today()).dt.days / 365
+    
+    # Boolean column if the option is a CALL
+    options['CALL'] = options['contractSymbol'].str[4:].apply(
+        lambda x: "C" in x)
+    
+    options[['bid', 'ask', 'strike']] = options[['bid', 'ask', 'strike']].apply(pd.to_numeric)
+    options['mark'] = (options['bid'] + options['ask']) / 2 # Calculate the midpoint of the bid-ask
+    
+    # Drop unnecessary and meaningless columns
+    options = options.drop(columns = ['contractSize', 'currency', 'change', 'percentChange', 'lastTradeDate', 'lastPrice'])
+
+    return options
+
+
+
+
+
+st.set_page_config(
+    page_title="Options",
+    page_icon="🧊",
+    layout="wide")
+
+ticker = st.text_input("Enter ticker", "Type Here")
+df = options_chain(ticker)
+st.dataframe(df)
+
+pass
